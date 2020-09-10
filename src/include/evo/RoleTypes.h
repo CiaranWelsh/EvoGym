75247a7f1ea599368029f81fb3bd38bc5e5cedee
Ciaran Welsh <cwelsh2@uw.edu>
2020-09-10 09:09:50 +0100

    Add role type enum

---
+++ src/include/evo/RoleTypes.h
@@ -0,0 +1,17 @@
+//
+// Created by Ciaran on 08/09/2020.
+//
+
+#ifndef EVOGYM_ROLETYPES_H
+#define EVOGYM_ROLETYPES_H
+
+namespace evo {
+    enum RoleType {
+        EVO_SUBSTRATE,
+        EVO_PRODUCT,
+        EVO_PARAMETER,
+        EVO_MODIFIER
+    };
+}
+
+#endif //EVOGYM_ROLETYPES_H
